45556369a94f633b31d83ebc3634283ef4478a71
Heinrich Klobuczek <heinrich@mail.com>
2010-01-13 22:30:12 -0800

    added convenient config for rackup

--- lib/neo4jr/configuration.rb
+++ lib/neo4jr/configuration.rb
@@ -2,7 +2,7 @@ module Neo4jr
   class Configuration
     class << self
       def database_path
-        @database_path ||= (System.getProperty('neo4jr.database') || Dir.mktmpdir)
+        @database_path ||= (System.getProperty('neo4jr.database') || ENV['neo4jr_database'] || Dir.mktmpdir)
       end
     
       def database_path=(value)
