e6f64d50b05c3d06735da82b4ce1f01e6cbfb47f
Matthew Deiters <bangkok@Matthew-Deiterss-MacBook-Pro.local>
2010-01-26 16:22:37 -0800

    updated pathrater

--- lib/neo4jr/path_rater.rb
+++ lib/neo4jr/path_rater.rb
@@ -4,7 +4,7 @@ module Neo4jr
       cost = start_cost
       previous = nil
       path.each do |r|
-        cost = cost_accumulator.addCosts(cost, cost_evaluator.getCost(r, r.getEndNode == previous)) if r.kind_of? org.neo4j.api.core.Relationship
+        cost = cost_accumulator.addCosts(cost, cost_evaluator.getCost(r, r.getEndNode == previous)) if r.kind_of? org.neo4j.graphdb.Relationship
         previous = r
       end
       cost
